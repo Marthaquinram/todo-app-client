db0500952d61258f97f0d4d91e6031f91d61c85c
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2023-01-12 23:34:32 -0800

    started front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LoginButton from "./auth/LoginButton"
+import LogoutButton from "./auth/LogoutButton";
+import Profile from "./auth/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+
+//pages & components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+
+
 
 function App() {
+  const { isLoading, error } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="column">
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <div className="pages">
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
+    </main>
+
   );
 }
 
